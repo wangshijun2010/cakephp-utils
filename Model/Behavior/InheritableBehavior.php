cb0df2f75c5f877e15ad261fb82fcecd4625d99d
wangshijun2010 <wangshijun2010@gmail.com>
2014-02-09 09:58:29 +0800

    fix php compatibility issue

--- Model/Behavior/InheritableBehavior.php
+++ Model/Behavior/InheritableBehavior.php
@@ -2,13 +2,13 @@
 /**
  * This code is an improved version of the Inheritable Behavior published
  * at http://bakery.cakephp.org/articles/view/inheritable-behavior-missing-link-of-cake-model
- * 
+ *
  * It was itself based on top of the SubclassBehavior by Eldon Bite <eldonbite@gmail.com>
  * and the ExtendableBehavior class by Matthew Harris which can be found at
- * http://bakery.cakephp.org/articles/view/extendablebehavior 
- * 
+ * http://bakery.cakephp.org/articles/view/extendablebehavior
+ *
  * @author Cake Development Corporation (http://cakedc.com)
- * @license   http://www.opensource.org/licenses/mit-license.php The MIT License 
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 
 /**
@@ -26,13 +26,13 @@ class InheritableBehavior extends ModelBehavior {
  * Each settings are keyed by Model alias.
  *
  * The available settings keys are:
- * 	- inheritanceField
- * 	- method: STI or CTI
- * 	- fieldAlias
+ *  - inheritanceField
+ *  - method: STI or CTI
+ *  - fieldAlias
  *
  * @var array
  */
-	public $settings = array();
+    public $settings = array();
 
 /**
  * Set up the behavior.
@@ -43,21 +43,21 @@ class InheritableBehavior extends ModelBehavior {
  * @internal param \Model $model
  * @return void
  */
-	public function setup(Model $Model, $config = array()) {
-		$_defaults = array(
-			'inheritanceField' => 'type',
-			'method' => 'STI',
-			'fieldAlias' => $Model->alias);
-		$this->settings[$Model->alias] = array_merge($_defaults, $config);
+    public function setup(Model $Model, $config = array()) {
+        $_defaults = array(
+            'inheritanceField' => 'type',
+            'method' => 'STI',
+            'fieldAlias' => $Model->alias);
+        $this->settings[$Model->alias] = array_merge($_defaults, $config);
 
-		$Model->parent = ClassRegistry::init(get_parent_class($Model));
-		$Model->inheritanceField = $this->settings[$Model->alias]['inheritanceField'];
-		$Model->fieldAlias = $this->settings[$Model->alias]['fieldAlias'];
+        $Model->parent = ClassRegistry::init(get_parent_class($Model));
+        $Model->inheritanceField = $this->settings[$Model->alias]['inheritanceField'];
+        $Model->fieldAlias = $this->settings[$Model->alias]['fieldAlias'];
 
-		if ($this->settings[$Model->alias]['method'] == 'CTI') {
-			$this->classTableBindParent($Model);
-		}
-	}
+        if ($this->settings[$Model->alias]['method'] == 'CTI') {
+            $this->classTableBindParent($Model);
+        }
+    }
 
 /**
  * Before find callback
@@ -67,23 +67,23 @@ class InheritableBehavior extends ModelBehavior {
  * @param array $query Find query
  * @return array Updated query
  */
-	public function beforeFind(Model $Model, $query) {
-		if ($this->settings[$Model->alias]['method'] == 'STI') {
-			$query = $this->_singleTableBeforeFind($Model, $query);
-		} else {
-			if (!empty($query['recursive']) && $query['recursive'] < 0) {
-				unset($query['recursive']);
-				$query['contain'] = array($Model->parent->alias);
-			}
-			if (!empty($query['contain'])) {
-				$this->_classTableBindContains($Model, $query['contain']);
-				if (empty($query['contain'][$Model->parent->alias]) && !in_array($Model->parent->alias, $query['contain'])) {
-					array_unshift($query['contain'], $Model->parent->alias);
-				}
-			}
-		}
-		return $query;
-	}
+    public function beforeFind(Model $Model, $query) {
+        if ($this->settings[$Model->alias]['method'] == 'STI') {
+            $query = $this->_singleTableBeforeFind($Model, $query);
+        } else {
+            if (!empty($query['recursive']) && $query['recursive'] < 0) {
+                unset($query['recursive']);
+                $query['contain'] = array($Model->parent->alias);
+            }
+            if (!empty($query['contain'])) {
+                $this->_classTableBindContains($Model, $query['contain']);
+                if (empty($query['contain'][$Model->parent->alias]) && !in_array($Model->parent->alias, $query['contain'])) {
+                    array_unshift($query['contain'], $Model->parent->alias);
+                }
+            }
+        }
+        return $query;
+    }
 
 /**
  * After find callback
@@ -94,41 +94,41 @@ class InheritableBehavior extends ModelBehavior {
  * @param boolean $primary
  * @return array Results
  */
-	public function afterFind(Model $Model, $results = array(), $primary = false) {
-		if ($this->settings[$Model->alias] !== 'STI' && !empty($results)) {
-			foreach ($results as $i => $res) {
-				if (is_int($i)) {
-					if (!empty($res[$Model->parent->alias]) && !empty($res[$Model->alias])) {
-						$results[$i][$Model->alias] = array_merge($res[$Model->parent->alias], $res[$Model->alias]);
-						unset($results[$i][$Model->parent->alias]);
-	
-					} elseif (!empty($res[$Model->alias][$Model->parent->alias])) {
-						$results[$i][$Model->alias] = array_merge($res[$Model->alias][$Model->parent->alias], $res[$Model->alias]);
-						unset($results[$i][$Model->alias][$Model->parent->alias]);
-	
-					} elseif (!empty($res[$Model->alias][0])) {
-						foreach($res[$Model->alias] as $j => $subRes) {
-							if (isset($subRes[$Model->parent->alias])) {
-								$results[$i][$Model->alias][$j] = array_merge($subRes[$Model->parent->alias], $subRes);
-								unset($results[$i][$Model->alias][$j][$Model->parent->alias]);
-							}
-						}
-					}
-				} elseif ($i == $Model->parent->alias) {
-					$results = array_merge($results, $res);
-					unset($results[$i]);
-				} elseif ($i == $Model->alias && array_key_exists(0, $res)) {
-					foreach($res as $j => $payload) {
-						if (array_key_exists($Model->parent->alias, $payload)) {
-							$results[$i][$j] = array_merge($payload, $payload[$Model->parent->alias]);
-							unset($results[$i][$j][$Model->parent->alias]);
-						}
-					}
-				}
-			}
-		}
-		return $results;
-	}
+    public function afterFind(Model $Model, $results = array(), $primary = false) {
+        if ($this->settings[$Model->alias] !== 'STI' && !empty($results)) {
+            foreach ($results as $i => $res) {
+                if (is_int($i)) {
+                    if (!empty($res[$Model->parent->alias]) && !empty($res[$Model->alias])) {
+                        $results[$i][$Model->alias] = array_merge($res[$Model->parent->alias], $res[$Model->alias]);
+                        unset($results[$i][$Model->parent->alias]);
+
+                    } elseif (!empty($res[$Model->alias][$Model->parent->alias])) {
+                        $results[$i][$Model->alias] = array_merge($res[$Model->alias][$Model->parent->alias], $res[$Model->alias]);
+                        unset($results[$i][$Model->alias][$Model->parent->alias]);
+
+                    } elseif (!empty($res[$Model->alias][0])) {
+                        foreach($res[$Model->alias] as $j => $subRes) {
+                            if (isset($subRes[$Model->parent->alias])) {
+                                $results[$i][$Model->alias][$j] = array_merge($subRes[$Model->parent->alias], $subRes);
+                                unset($results[$i][$Model->alias][$j][$Model->parent->alias]);
+                            }
+                        }
+                    }
+                } elseif ($i == $Model->parent->alias) {
+                    $results = array_merge($results, $res);
+                    unset($results[$i]);
+                } elseif ($i == $Model->alias && array_key_exists(0, $res)) {
+                    foreach($res as $j => $payload) {
+                        if (array_key_exists($Model->parent->alias, $payload)) {
+                            $results[$i][$j] = array_merge($payload, $payload[$Model->parent->alias]);
+                            unset($results[$i][$j][$Model->parent->alias]);
+                        }
+                    }
+                }
+            }
+        }
+        return $results;
+    }
 
 /**
  * Before save callback
@@ -137,15 +137,15 @@ class InheritableBehavior extends ModelBehavior {
  * @param Model $Model
  * @return true
  */
-	public function beforeSave(Model $Model) {
-		if ($this->settings[$Model->alias]['method'] == 'STI') {
-			$this->_singleTableBeforeSave($Model);
-		} elseif ($this->settings[$Model->alias]['method'] == 'CTI') {
-			$this->_saveParentModel($Model);
-			$Model->id = $Model->parent->id;
-		}
-		return true;
-	}
+    public function beforeSave(Model $Model, $options = array()) {
+        if ($this->settings[$Model->alias]['method'] == 'STI') {
+            $this->_singleTableBeforeSave($Model);
+        } elseif ($this->settings[$Model->alias]['method'] == 'CTI') {
+            $this->_saveParentModel($Model);
+            $Model->id = $Model->parent->id;
+        }
+        return true;
+    }
 
 /**
  * After delete callback
@@ -154,12 +154,12 @@ class InheritableBehavior extends ModelBehavior {
  * @param Model $Model
  * @return true
  */
-	public function afterDelete(Model $Model) {
-		if ($this->settings[$Model->alias]['method'] == 'CTI') {
-			$Model->parent->delete($Model->id);
-		}
-		return true;
-	}
+    public function afterDelete(Model $Model) {
+        if ($this->settings[$Model->alias]['method'] == 'CTI') {
+            $Model->parent->delete($Model->id);
+        }
+        return true;
+    }
 
 /**
  * Before validate callback
@@ -168,12 +168,12 @@ class InheritableBehavior extends ModelBehavior {
  * @param Model $model
  * @return true
  */
-	public function beforeValidate(Model $Model) {
-		if ($this->settings[$Model->alias]['method'] == 'CTI' && !empty($Model->parent->validate)) {
-			$Model->validate = Set::merge($Model->parent->validate, $Model->validate);
-		}
-		return true;
-	}
+    public function beforeValidate(Model $Model, $options = array()) {
+        if ($this->settings[$Model->alias]['method'] == 'CTI' && !empty($Model->parent->validate)) {
+            $Model->validate = Set::merge($Model->parent->validate, $Model->validate);
+        }
+        return true;
+    }
 
 /**
  * Beforefind callback for STI models
@@ -182,28 +182,28 @@ class InheritableBehavior extends ModelBehavior {
  * @param array $query Find query
  * @return Updated query
  */
-	protected function _singleTableBeforeFind(Model $Model, $query) {
-		extract($this->settings[$Model->alias]);
-		$_schema = $Model->schema();
-		if (isset($_schema[$inheritanceField]) && $Model->alias != $Model->parent->alias) {
-			$field = $Model->alias. '.' . $inheritanceField;
+    protected function _singleTableBeforeFind(Model $Model, $query) {
+        extract($this->settings[$Model->alias]);
+        $_schema = $Model->schema();
+        if (isset($_schema[$inheritanceField]) && $Model->alias != $Model->parent->alias) {
+            $field = $Model->alias. '.' . $inheritanceField;
 
-			if (!isset($query['conditions'])) {
-				$query['conditions'] = array();
-			} elseif (is_string($query['conditions'])) {
-				$query['conditions'] = array($query['conditions']);
-			}
+            if (!isset($query['conditions'])) {
+                $query['conditions'] = array();
+            } elseif (is_string($query['conditions'])) {
+                $query['conditions'] = array($query['conditions']);
+            }
 
-			if (is_array($query['conditions'])) {
-				if (!isset($query['conditions'][$field])) {
-					$query['conditions'][$field] = array();
-				}
-				$query['conditions'][$field] = $fieldAlias;
-			}
-		}
+            if (is_array($query['conditions'])) {
+                if (!isset($query['conditions'][$field])) {
+                    $query['conditions'][$field] = array();
+                }
+                $query['conditions'][$field] = $fieldAlias;
+            }
+        }
 
-		return $query;
-	}
+        return $query;
+    }
 
 /**
  * BeforeSave callback for STI models
@@ -212,16 +212,16 @@ class InheritableBehavior extends ModelBehavior {
  * @param Model $Model
  * @return true
  */
-	protected function _singleTableBeforeSave(Model $Model) {
-		if (isset($Model->_schema[$Model->inheritanceField]) && $Model->alias != $Model->parent->alias) {
-			// May be there is an edge case for this
-			if (!isset($Model->data[$Model->alias])) {
-				$Model->data[$Model->alias] = array();
-			}
-			$Model->data[$Model->alias][$Model->inheritanceField] = $Model->alias;
-		}
-		return true;
-	}
+    protected function _singleTableBeforeSave(Model $Model) {
+        if (isset($Model->_schema[$Model->inheritanceField]) && $Model->alias != $Model->parent->alias) {
+            // May be there is an edge case for this
+            if (!isset($Model->data[$Model->alias])) {
+                $Model->data[$Model->alias] = array();
+            }
+            $Model->data[$Model->alias][$Model->inheritanceField] = $Model->alias;
+        }
+        return true;
+    }
 
 /**
  * Binds the parent model for a CTI model
@@ -230,20 +230,20 @@ class InheritableBehavior extends ModelBehavior {
  * @param array $query
  * @return boolean Success of the binding
  */
-	public function classTableBindParent(Model $Model) {
-		$bind = array('belongsTo' => array(
-			$Model->parent->alias => array(
-				'type' => 'INNER',
-				'className' => $Model->parent->alias,
-				'foreignKey' => $Model->primaryKey)));
-		$success = $Model->bindModel($bind, false);
-		//Putting the parent association as the first one, so any dependent join on the parent model will
-		// be in the right order
-		$assoc = $Model->belongsTo[$Model->parent->alias];
-		unset($Model->belongsTo[$Model->parent->alias]);
-		$Model->belongsTo = array_merge(array($Model->parent->alias => $assoc), $Model->belongsTo);
-		return $success;
-	}
+    public function classTableBindParent(Model $Model) {
+        $bind = array('belongsTo' => array(
+            $Model->parent->alias => array(
+                'type' => 'INNER',
+                'className' => $Model->parent->alias,
+                'foreignKey' => $Model->primaryKey)));
+        $success = $Model->bindModel($bind, false);
+        //Putting the parent association as the first one, so any dependent join on the parent model will
+        // be in the right order
+        $assoc = $Model->belongsTo[$Model->parent->alias];
+        unset($Model->belongsTo[$Model->parent->alias]);
+        $Model->belongsTo = array_merge(array($Model->parent->alias => $assoc), $Model->belongsTo);
+        return $success;
+    }
 
 /**
  * Binds additional belongsTo association from the parent for a CTI model
@@ -251,22 +251,22 @@ class InheritableBehavior extends ModelBehavior {
  * @param Model $Model
  * @param array $binds, additional models to bind. They will be filtered to left only belongsTo associations
  */
-	protected function _classTableBindContains(Model $Model, $binds) {
-		$assocs = array_flip($Model->parent->getAssociated('belongsTo'));
-		foreach ($binds as $k => $alias) {
-			if (is_array($alias)) {
-				$alias = $k;
-			}
-			if (isset($assocs[$alias])) {
-				$foreignKey = Inflector::underscore($alias) . '_id';
-				$bind = array('belongsTo' => array(
-					$alias => array(
-						'conditions' => "{$Model->parent->alias}.{$foreignKey} = {$alias}.id",
-						'foreignKey' => false)));
-				$Model->bindModel($bind, true);
-			}
-		}
-	}
+    protected function _classTableBindContains(Model $Model, $binds) {
+        $assocs = array_flip($Model->parent->getAssociated('belongsTo'));
+        foreach ($binds as $k => $alias) {
+            if (is_array($alias)) {
+                $alias = $k;
+            }
+            if (isset($assocs[$alias])) {
+                $foreignKey = Inflector::underscore($alias) . '_id';
+                $bind = array('belongsTo' => array(
+                    $alias => array(
+                        'conditions' => "{$Model->parent->alias}.{$foreignKey} = {$alias}.id",
+                        'foreignKey' => false)));
+                $Model->bindModel($bind, true);
+            }
+        }
+    }
 
 /**
  * After save callback for CTI models
@@ -275,21 +275,21 @@ class InheritableBehavior extends ModelBehavior {
  * @param Model $Model
  * @return true
  */
-	protected function _saveParentModel(Model $Model) {
-		$fields = array_keys($Model->parent->schema());
-		$parentData = array($Model->parent->primaryKey => $Model->id);
+    protected function _saveParentModel(Model $Model) {
+        $fields = array_keys($Model->parent->schema());
+        $parentData = array($Model->parent->primaryKey => $Model->id);
 
-		foreach ($Model->data[$Model->alias] as $key => $value) {
-			if (in_array($key, $fields)) {
-				$parentData[$key] = $value;
-			}
-		}
+        foreach ($Model->data[$Model->alias] as $key => $value) {
+            if (in_array($key, $fields)) {
+                $parentData[$key] = $value;
+            }
+        }
 
-		$result = $Model->parent->save($parentData, false);
-		if ($result !== false) {
-			$Model->data[$Model->alias] = Set::merge($Model->data[$Model->alias], $result[$Model->parent->alias]);
-		}
-		return true;
-	}
+        $result = $Model->parent->save($parentData, false);
+        if ($result !== false) {
+            $Model->data[$Model->alias] = Set::merge($Model->data[$Model->alias], $result[$Model->parent->alias]);
+        }
+        return true;
+    }
 
 }
--- Model/Behavior/SluggableBehavior.php
+++ Model/Behavior/SluggableBehavior.php
@@ -27,33 +27,33 @@ class SluggableBehavior extends ModelBehavior {
  *
  * @var array
  */
-	public $settings = array();
+    public $settings = array();
 
 /**
  * Default settings
  *
- * label 		- The field used to generate the slug from
- * slug 		- The field to store the slug in
- * scope 		- conditions for the find query to check if the slug already exists
- * separator 	- the character used to separate the words in the slug
- * length		- the maximum length of the slug
- * unique		- check if the slug is unique
- * update		- update the slug or not
- * trigger		- defines a property in the model that has to be true to generate the slug
+ * label        - The field used to generate the slug from
+ * slug         - The field to store the slug in
+ * scope        - conditions for the find query to check if the slug already exists
+ * separator    - the character used to separate the words in the slug
+ * length       - the maximum length of the slug
+ * unique       - check if the slug is unique
+ * update       - update the slug or not
+ * trigger      - defines a property in the model that has to be true to generate the slug
  *
  * Note that trigger will temporary bypass update and act like update is set to true.
  *
  * @var array
  */
-	protected $_defaults = array(
-		'label' => 'title',
-		'slug' => 'slug',
-		'scope' => array(),
-		'separator' => '_',
-		'length' => 255,
-		'unique' => true,
-		'update' => false,
-		'trigger' => false);
+    protected $_defaults = array(
+        'label' => 'title',
+        'slug' => 'slug',
+        'scope' => array(),
+        'separator' => '_',
+        'length' => 255,
+        'unique' => true,
+        'update' => false,
+        'trigger' => false);
 
 /**
  * Initiate behaviour
@@ -61,53 +61,53 @@ class SluggableBehavior extends ModelBehavior {
  * @param object $Model
  * @param array $settings
  */
-	public function setup(Model $Model, $settings = array()) {
-		$this->settings[$Model->alias] = array_merge($this->_defaults, $settings);
-	}
+    public function setup(Model $Model, $settings = array()) {
+        $this->settings[$Model->alias] = array_merge($this->_defaults, $settings);
+    }
 
 /**
  * beforeSave callback
  *
  * @param object $Model
  */
-	public function beforeSave(Model $Model) {
-		$settings = $this->settings[$Model->alias];
-		if (is_string($this->settings[$Model->alias]['trigger'])) {
-			if ($Model->{$this->settings[$Model->alias]['trigger']} != true) {
-				return true;
-			}
-		}
+    public function beforeSave(Model $Model, $options = array()) {
+        $settings = $this->settings[$Model->alias];
+        if (is_string($this->settings[$Model->alias]['trigger'])) {
+            if ($Model->{$this->settings[$Model->alias]['trigger']} != true) {
+                return true;
+            }
+        }
 
-		if (empty($Model->data[$Model->alias])) {
-			return true;
-		} else if (empty($Model->data[$Model->alias][$this->settings[$Model->alias]['label']])) {
-			return true;
-		} else if (!$this->settings[$Model->alias]['update'] && !empty($Model->id) && !is_string($this->settings[$Model->alias]['trigger'])) {
-			return true;
-		}
+        if (empty($Model->data[$Model->alias])) {
+            return true;
+        } else if (empty($Model->data[$Model->alias][$this->settings[$Model->alias]['label']])) {
+            return true;
+        } else if (!$this->settings[$Model->alias]['update'] && !empty($Model->id) && !is_string($this->settings[$Model->alias]['trigger'])) {
+            return true;
+        }
 
-		$slug = $Model->data[$Model->alias][$settings['label']];
-		if (method_exists($Model, 'beforeSlugGeneration')) {
-			$slug = $Model->beforeSlugGeneration($slug, $settings['separator']);
-		}
+        $slug = $Model->data[$Model->alias][$settings['label']];
+        if (method_exists($Model, 'beforeSlugGeneration')) {
+            $slug = $Model->beforeSlugGeneration($slug, $settings['separator']);
+        }
 
-		$settings = $this->settings[$Model->alias];
-		if (method_exists($Model, 'multibyteSlug')) {
-			$slug = $Model->multibyteSlug($slug, $settings['separator']);
-		} else {
-			$slug = $this->multibyteSlug($Model, $slug);
-		}
+        $settings = $this->settings[$Model->alias];
+        if (method_exists($Model, 'multibyteSlug')) {
+            $slug = $Model->multibyteSlug($slug, $settings['separator']);
+        } else {
+            $slug = $this->multibyteSlug($Model, $slug);
+        }
 
-		if ($settings['unique'] === true || is_array($settings['unique'])) {
-			$slug = $this->makeUniqueSlug($Model, $slug);
-		}
+        if ($settings['unique'] === true || is_array($settings['unique'])) {
+            $slug = $this->makeUniqueSlug($Model, $slug);
+        }
 
-		if (!empty($Model->whitelist) && !in_array($settings['slug'], $Model->whitelist)) {
-			$Model->whitelist[] = $settings['slug'];
-		}
-		$Model->data[$Model->alias][$settings['slug']] = $slug;
-		return true;
-	}
+        if (!empty($Model->whitelist) && !in_array($settings['slug'], $Model->whitelist)) {
+            $Model->whitelist[] = $settings['slug'];
+        }
+        $Model->data[$Model->alias][$settings['slug']] = $slug;
+        return true;
+    }
 
 /**
  * Searche if the slug already exists and if yes increments it
@@ -117,48 +117,48 @@ class SluggableBehavior extends ModelBehavior {
  * @return string The incremented unique slug
  *
  */
-	public function makeUniqueSlug(Model $Model, $slug = '') {
-		$settings = $this->settings[$Model->alias];
-		$conditions = array();
-		if ($settings['unique'] === true) {
-			$conditions[$Model->alias . '.' . $settings['slug'] . ' LIKE'] = $slug . '%';
-		} else if (is_array($settings['unique'])) {
-			foreach ($settings['unique'] as $field) {
-				$conditions[$Model->alias . '.' . $field] = $Model->data[$Model->alias][$field];
-			}
-			$conditions[$Model->alias . '.' . $settings['slug'] . ' LIKE'] = $slug . '%';
-		}
+    public function makeUniqueSlug(Model $Model, $slug = '') {
+        $settings = $this->settings[$Model->alias];
+        $conditions = array();
+        if ($settings['unique'] === true) {
+            $conditions[$Model->alias . '.' . $settings['slug'] . ' LIKE'] = $slug . '%';
+        } else if (is_array($settings['unique'])) {
+            foreach ($settings['unique'] as $field) {
+                $conditions[$Model->alias . '.' . $field] = $Model->data[$Model->alias][$field];
+            }
+            $conditions[$Model->alias . '.' . $settings['slug'] . ' LIKE'] = $slug . '%';
+        }
 
-		if (!empty($Model->id)) {
-			$conditions[$Model->alias . '.' . $Model->primaryKey . ' !='] = $Model->id;
-		}
+        if (!empty($Model->id)) {
+            $conditions[$Model->alias . '.' . $Model->primaryKey . ' !='] = $Model->id;
+        }
 
-		$conditions = array_merge($conditions, $settings['scope']);
+        $conditions = array_merge($conditions, $settings['scope']);
 
-		$duplicates = $Model->find('all', array(
-			'recursive' => -1,
-			'conditions' => $conditions,
-			'fields' => array($settings['slug'])));
+        $duplicates = $Model->find('all', array(
+            'recursive' => -1,
+            'conditions' => $conditions,
+            'fields' => array($settings['slug'])));
 
-		if (!empty($duplicates)) {
-			$duplicates = Set::extract($duplicates, '{n}.' . $Model->alias . '.' . $settings['slug']);
-			if (!in_array($slug, $duplicates)) {
-				return $slug;
-			}
+        if (!empty($duplicates)) {
+            $duplicates = Set::extract($duplicates, '{n}.' . $Model->alias . '.' . $settings['slug']);
+            if (!in_array($slug, $duplicates)) {
+                return $slug;
+            }
 
-			$startSlug = $slug;
-			$index = 1;
+            $startSlug = $slug;
+            $index = 1;
 
-			while ($index > 0) {
-				if (!in_array($startSlug . $settings['separator'] . $index, $duplicates)) {
-					$slug = $startSlug . $settings['separator'] . $index;
-					$index = -1;
-				}
-				$index++;
-			}
-		}
-		return $slug;
-	}
+            while ($index > 0) {
+                if (!in_array($startSlug . $settings['separator'] . $index, $duplicates)) {
+                    $slug = $startSlug . $settings['separator'] . $index;
+                    $index = -1;
+                }
+                $index++;
+            }
+        }
+        return $slug;
+    }
 
 /**
  * Generates a slug from a (multibyte) string
@@ -167,15 +167,15 @@ class SluggableBehavior extends ModelBehavior {
  * @param string $string
  * @return string
  */
-	public function multibyteSlug(Model $Model, $string = null) {
-		$str = mb_strtolower($string);
-		$str = preg_replace('/\xE3\x80\x80/', ' ', $str);
-		$str = preg_replace('[\'s ]', 's ', $str);
-		$str = str_replace($this->settings[$Model->alias]['separator'], ' ', $str);
-		$str = preg_replace( '#[:\#\*"()~$^{}`@+=;,<>!&%\.\]\/\'\\\\|\[]#', "\x20", $str );
-		$str = str_replace('?', '', $str);
-		$str = trim($str);
-		$str = preg_replace('#\x20+#', $this->settings[$Model->alias]['separator'], $str);
-		return $str;
-	}
+    public function multibyteSlug(Model $Model, $string = null) {
+        $str = mb_strtolower($string);
+        $str = preg_replace('/\xE3\x80\x80/', ' ', $str);
+        $str = preg_replace('[\'s ]', 's ', $str);
+        $str = str_replace($this->settings[$Model->alias]['separator'], ' ', $str);
+        $str = preg_replace( '#[:\#\*"()~$^{}`@+=;,<>!&%\.\]\/\'\\\\|\[]#', "\x20", $str );
+        $str = str_replace('?', '', $str);
+        $str = trim($str);
+        $str = preg_replace('#\x20+#', $this->settings[$Model->alias]['separator'], $str);
+        return $str;
+    }
 }
